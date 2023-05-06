9acbb02a982101c308d2c33e8b2435d021d22069
3djc <lesitewebdejc@hotmail.com>
2023-05-06 08:48:30 +0200

    Fix DEBUG compile

--- radio/src/translations/fr.h
+++ radio/src/translations/fr.h
@@ -119,6 +119,7 @@
 #define TR_SF_SAFETY                   TR("Rempl.", "Remplace")
 
 #define TR_SF_SCREENSHOT               "Photo Ecran"
+#define TR_SF_TEST                     "Test"
 #define TR_SF_RACING_MODE              "Racing Mode"
 #define TR_SF_DISABLE_TOUCH            "Non Tactile"
 #define TR_SF_SET_SCREEN               "Set Main Screen"
